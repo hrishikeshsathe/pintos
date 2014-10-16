ee08bbb5b2634df08c2266c59157a64f91fa8e23
hrishikeshsathe <xxx2050@gmail.com>
2014-10-16 00:42:41 -0400

    14 of 27 - priority-condvar

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -41,6 +41,8 @@
 
    - up or "V": increment the value (and wake up one waiting
      thread, if any). */
+bool semaphore_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
+
 void
 sema_init (struct semaphore *sema, unsigned value) 
 {
@@ -306,6 +308,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   
   sema_init (&waiter.semaphore, 0);
   list_push_back (&cond->waiters, &waiter.elem);
+  list_sort(&cond->waiters,(list_less_func *) &semaphore_priority, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
@@ -327,6 +330,7 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (lock_held_by_current_thread (lock));
 
   if (!list_empty (&cond->waiters)) 
+  list_sort(&cond->waiters,(list_less_func *) &semaphore_priority, NULL);
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
 }
@@ -345,4 +349,35 @@ cond_broadcast (struct condition *cond, struct lock *lock)
 
   while (!list_empty (&cond->waiters))
     cond_signal (cond, lock);
+}
+
+/*added functions*/
+bool semaphore_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED)
+{
+  struct semaphore_elem *sema_a = list_entry(a, struct semaphore_elem, elem);
+  struct semaphore_elem *sema_b = list_entry(b, struct semaphore_elem, elem);
+  
+  if(list_empty(&sema_b->semaphore.waiters))
+  {
+     return true;
+  }//endif
+  if(list_empty(&sema_a->semaphore.waiters))
+  {
+     return false;
+  }//endif
+  
+  list_sort(&sema_a->semaphore.waiters, (list_less_func *) &compare_priority,
+NULL);
+list_sort(&sema_b->semaphore.waiters, (list_less_func *) &compare_priority,
+NULL);
+struct thread *ta = list_entry(list_front(&sema_a->semaphore.waiters),
+struct thread, elem);
+struct thread *tb = list_entry(list_front(&sema_b->semaphore.waiters),
+struct thread, elem);
+if (ta->priority > tb->priority)
+{
+return true;
+}
+return false; 
+     
 }
